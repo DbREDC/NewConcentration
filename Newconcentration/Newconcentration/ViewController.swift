4cc3d80f0d2f8bddb6d964c3e7d2b6d21eeca478
孙鹏鹏 <>
2018-12-19 16:05:38 +0800

    第一节课
    学习如何手工添加button，并在viewController内进行button的点击事件处理（模拟翻牌操作），加入emoji数组并进行乱序处理，加入UILabel统计点击次数。

--- Newconcentration/Newconcentration/ViewController.swift
+++ Newconcentration/Newconcentration/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var emojiGroup = ["😈","👻","💄","💍","🐶","🐷","🐸","🐵","🐥","🦄","🐠","🦀","🐿","🍄","🍎","🥝","🌽","🍞","🍔","🍙","🏀","🎲","🚗","🚄","⌚️","⏰"]
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "FlipCount : \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var buttonGroup: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = buttonGroup.index(of : sender){
+            flipCount += 1
+            filpCard(withEmoji: emojiGroup[cardNumber], on: sender)
+        }
+    }
+    
+    func filpCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle(" ", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        shuffleEmoji()
     }
-
-
+    
+    func shuffleEmoji() {
+        let emojiNumber = emojiGroup.count
+        for i in 0..<emojiNumber {
+            let j = Int(arc4random_uniform(UInt32(emojiNumber-i)))+i
+            if i != j {
+                (emojiGroup[i],emojiGroup[j]) = (emojiGroup[j],emojiGroup[i])
+            }
+        }
+    }
+    
 }
 
